f2ce651967545cfb37d370f0094e7f06578ae8e6
Kunal Khatri <kunal15.khatri@gmail.com>
2016-07-06 00:01:36 +0530

    Adding the back button in Info acivity.

--- app/src/main/java/com/khatrigmail/kunal15/recyclerview/InfoActivity.java
+++ app/src/main/java/com/khatrigmail/kunal15/recyclerview/InfoActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class InfoActivity extends AppCompatActivity {
@@ -16,11 +17,22 @@ public class InfoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_info);
         t = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(t);
-        getSupportActionBar().setTitle("InfoActivity");
+        getSupportActionBar().setTitle(InfoActivity.class.getSimpleName());
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         Intent i = getIntent();
         name = (TextView) findViewById(R.id.tvName);
         number = (TextView) findViewById(R.id.tvNumber);
         name.setText(i.getStringExtra("Name"));
         number.setText(i.getStringExtra("Number"));
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case android.R.id.home:
+                Intent homeIntent = new Intent(this, MainActivity.class);
+                startActivity(homeIntent);
+        }
+        return true;
     }
 }
